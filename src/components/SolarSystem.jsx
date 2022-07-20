6354ea32b2dea4786178ac10093fcecb43e65a8a
Bianca Oura <biancaoura@gmail.com>
2022-07-20 16:55:48 -0300

    Requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
+    const renderPlanets = planets.map((planet, index) => (
+      <PlanetCard key={ index } planetName={ planet.name } planetImage={ planet.image } />
+    ));
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {renderPlanets}
       </div>
     );
   }
