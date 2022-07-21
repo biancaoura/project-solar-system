def3646e07efccf821aab6263a1287b6773bfe8e
Bianca Oura <biancaoura@gmail.com>
2022-07-21 12:31:42 -0300

    Estiliza header

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,7 +5,7 @@ class Title extends Component {
   render() {
     const { headline } = this.props;
     return (
-      <h2>{headline}</h2>
+      <h2 className="title">{headline}</h2>
     );
   }
 }
@@ -15,7 +15,7 @@ Title.propTypes = {
 };
 
 Title.defaultProps = {
-  headline: 'Planetas',
+  headline: 'PLANETAS',
 };
 
 export default Title;
