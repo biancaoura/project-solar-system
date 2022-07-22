97a64174c90cbc5e433773581c984649c07e8b47
Bianca Oura <biancaoura@gmail.com>
2022-07-22 12:46:09 -0300

    Adiciona animação

--- src/data/planets.js
+++ src/data/planets.js
@@ -12,7 +12,7 @@ const Planets = [
   { name: 'Vênus', image: VenusImg, size: 45 },
   { name: 'Terra', image: TerraImg, size: 50 },
   { name: 'Marte', image: MarteImg, size: 45 },
-  { name: 'Júpiter', image: JupiterImg, size: 500 },
+  { name: 'Júpiter', image: JupiterImg, size: 450 },
   { name: 'Saturno', image: SaturnoImg, size: 240 },
   { name: 'Urano', image: Uranoimg, size: 108 },
   { name: 'Netuno', image: Netunoimg, size: 105 },
