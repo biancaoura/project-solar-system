6f3ca3cb8073dcf28702a0918d233488a9e9d9bd
Bianca Oura <biancaoura@gmail.com>
2022-08-19 23:58:41 -0300

    altera exportação

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Header extends Component {
+export default class Header extends Component {
   render() {
     return (
       <header className="title">
@@ -10,4 +10,3 @@ class Header extends Component {
   }
 }
 
-export default Header;
